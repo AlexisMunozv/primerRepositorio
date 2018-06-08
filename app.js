5b00d1fd72c838f3752bf96f9f911631ed94e414
AlexisMunozv <alexismunozv@icloud.com>
2018-06-08 10:16:26 -0500

    Agregando lineas a Js y titulo h3 en html

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
       alert("Bienvenida al curso de JS");
       aleert("cambios en el js");
       alert('modificaciones');
+
+
+      holi
+
+      estamos aprendiendo a trabajar colaborativamente
